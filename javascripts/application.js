c1f24960338f6fb0c92e2640d3202001de4c436e
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-06-25 20:45:07 +1200

    created wall so that I have an intersection point at which to find an intersection point in the z plane. now creates random cylinders at position beneath click. uses raycaster intersection methods, also random colors for fun. need some refactoring.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,28 +1,10 @@
-var container, stats;
-var camera, scene, renderer;
-// var particleMaterial;
-
-var raycaster;
-var mouse;
-
 var objects = [];
 
-init();
-animate();
-
-function init(
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
-
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-  camera.position.set(0,300,500);
-
-  var geometry = new THREE.CylinderGeometry(100, 100, 247.49, 50, 50, false)
-
-  var material = new THREE.MeshBasicMaterial( {color: 0xd3d3d3} );
-  var marmite = new THREE.Mesh(geometry, material )
-  )
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
+camera.position.x = 0
+camera.position.y = 300
+camera.position.z = 1000
 
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2()
@@ -31,9 +13,14 @@ var renderer = new THREE.CanvasRenderer();
 renderer.setClearColor( 0xf0f0f0);
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-container.appendChild( renderer.domElement );
+document.body.appendChild( renderer.domElement );
 
 var controls = new THREE.OrbitControls( camera );
+
+var geometry = new THREE.BoxGeometry( 10000, 10000, 1 );
+var material = new THREE.MeshBasicMaterial( {color: 0xffffff, opacity: 0} );
+var wall = new THREE.Mesh(geometry, material )
+scene.add( wall )
 
 document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 document.addEventListener( 'touchstart', onDocumentTouchStart, false );
@@ -58,16 +45,6 @@ function onDocumentTouchStart( event ) {
 
 }
 
-// var newCube = function(xpos, ypos) {
-//   var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-//   var material = new THREE.MeshBasicMaterial( {color: 0xd3d3d3} );
-//   var cube = new THREE.Mesh( geometry, material );
-//   var cubeEdges = new THREE.EdgesHelper( cube, 0xffffff);
-//   scene.add( cube, cubeEdges );
-//   cube.position.x = xpos
-//   cube.position.y = ypos
-// }
-
 function onDocumentMouseDown( event ) {
 
   event.preventDefault();
@@ -77,20 +54,52 @@ function onDocumentMouseDown( event ) {
 
   raycaster.setFromCamera( mouse, camera );
 
-  var intersects = raycaster.intersectObjects( objects );
+  var intersects = raycaster.intersectObjects( [wall] );
 
   if ( intersects.length > 0 ) {
 
-    intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
 
-    var particle = new THREE.Sprite( particleMaterial );
-    particle.position.copy( intersects[ 0 ].point );
-    particle.scale.x = particle.scale.y = 16;
-    scene.add( particle );
+  var geometry = new THREE.CylinderGeometry(100, 100, 247.49, 50, 50, false)
+  var material = new THREE.MeshBasicMaterial( {color: 0xd3d3d3} );
+  var marmite = new THREE.Mesh(geometry, material )
+  var cylinderEdges = new THREE.EdgesHelper( marmite, 0xffffff);
+  marmite.material.color.setHex( Math.random() * 0xffffff );
+  marmite.position.copy(intersects[ 0 ].point )
+  marmite.scale = .5
+  scene.add( marmite, cylinderEdges );
 
   }
 
-$(document).on("click", function(e) {
+
+
+  // var intersects = raycaster.intersectObjects( objects );
+
+  // if ( intersects.length > 0 ) {
+
+
+  //   var particle = new THREE.Sprite( particleMaterial );
+  //   particle.position.copy( intersects[ 0 ].point );
+  //   particle.scale.x = particle.scale.y = 16;
+  //   scene.add( particle );
+
+  }
+
+function render() {
+  requestAnimationFrame( render );
+
+  // cube.rotation.x += 0.1;
+  // cube.rotation.y += 0.1;
+
+  renderer.render( scene, camera );
+}
+render();
+
+
+
+
+
+
+// $(document).on("click", function(e) {
 //   var xpos = e.pageX
 //   var ypos = e.pageY
 //   var geometry = new THREE.BoxGeometry( 50, 50, 50 );
@@ -106,15 +115,12 @@ $(document).on("click", function(e) {
 //   // console.log(newCube(xpos, ypos))
 // });
 
-
-
-
-function render() {
-  requestAnimationFrame( render );
-
-  // cube.rotation.x += 0.1;
-  // cube.rotation.y += 0.1;
-
-  renderer.render( scene, camera );
-}
-render();
+// var newCube = function(xpos, ypos) {
+//   var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//   var material = new THREE.MeshBasicMaterial( {color: 0xd3d3d3} );
+//   var cube = new THREE.Mesh( geometry, material );
+//   var cubeEdges = new THREE.EdgesHelper( cube, 0xffffff);
+//   scene.add( cube, cubeEdges );
+//   cube.position.x = xpos
+//   cube.position.y = ypos
+// }
